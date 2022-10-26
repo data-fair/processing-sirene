bc982ca81570d1a33188ddefd6efacf4a0b158b8
Alban Mouton <alban.mouton@gmail.com>
2022-10-26 09:20:35 +0200

    WIP

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ let _stopped
 // axios: an axios instance configured so that its base url is a data-fair instance and it sends proper credentials
 // log: contains async debug/info/warning/error methods to store a log on the processing run
 // patchConfig: async method accepting an object to be merged with the configuration
-exports.run = async ({ pluginConfig, processingConfig, processingId, dir, tmpDir, axios, log, patchConfig }) => {
+exports.run = async ({ pluginConfig, processingConfig, processingId, dir, tmpDir, axios, log, patchConfig, ws }) => {
   let dataset
   if (processingConfig.datasetMode === 'create') {
     await log.step('Création du jeu de données')
@@ -28,6 +28,7 @@ exports.run = async ({ pluginConfig, processingConfig, processingId, dir, tmpDir
     })).data
     await log.info(`jeu de donnée créé, id="${dataset.id}", title="${dataset.title}"`)
     await patchConfig({ datasetMode: 'update', dataset: { id: dataset.id, title: dataset.title } })
+    await ws.waitForJournal(dataset.id, 'finalize-end')
 
     log.step('Import du dernier fichier stock')
     if (_stopped) return await log.info('interruption demandée')
@@ -42,16 +43,45 @@ exports.run = async ({ pluginConfig, processingConfig, processingId, dir, tmpDir
     const form = new FormData()
     form.append('actions', fs.createReadStream(stockFilePath), 'stock.zip')
     const contentLength = await new Promise((resolve, reject) => form.getLength((err, length) => err ? reject(err) : resolve(length)))
-    await axios.post(`api/v1/datasets/${dataset.id}/_bulk_lines`, form, {
+    const bulkRes = (await axios.post(`api/v1/datasets/${dataset.id}/_bulk_lines`, form, {
       params: { lock: true },
       headers: { 'Content-Length': contentLength, ...form.getHeaders() }
-    })
+    })).data
+    const errors = bulkRes.errors
+    delete bulkRes.errors
+    log.info('réception des lignes du fichier de stock', bulkRes)
+    if (bulkRes.nbErrors) {
+      log.warning(`${bulkRes.nbErrors} lines were rejected with errors`, JSON.stringify(errors))
+    }
+    await ws.waitForJournal(dataset.id, 'finalize-end', 24 * 60 * 60 * 1000)
   } else if (processingConfig.datasetMode === 'update') {
     await log.step('Vérification du jeu de données')
     dataset = (await axios.get(`api/v1/datasets/${processingConfig.dataset.id}`)).data
     if (!dataset) throw new Error(`le jeu de données n'existe pas, id${processingConfig.dataset.id}`)
     await log.info(`le jeu de donnée existe, id="${dataset.id}", title="${dataset.title}"`)
   }
+
+  log.step('Récupération des modifications depuis la dernière date de traitement')
+  const lastLine = (await axios.get(`/api/v1/datasets/${dataset.id}/lines`, { params: { sort: '-dateDernierTraitementEtablissement', size: 1 } }))
+    .data.results[0]
+  if (!lastLine) throw new Error('Aucune ligne trouvée, le fichier stock n\'a pas du être correctement chargé.')
+  log.info('date du dernier traitement', lastLine.dateDernierTraitementEtablissement)
+
+  // cf https://api.insee.fr/catalogue/site/themes/wso2/subthemes/insee/pages/item-info.jag?name=Sirene&version=V3&provider=insee#!/Etablissement/findSiretByQ
+  const start = lastLine.dateDernierTraitementEtablissement.split('+')[0]
+  log.info(`interroge l'API Sirene après ${start}`)
+  const sireneApiRes = (await axios.get('https://api.insee.fr/entreprises/sirene/V3/siret', {
+    params: {
+      tri: 'dateDernierTraitementEtablissement',
+      q: `dateDernierTraitementEtablissement:[${start} TO *]`,
+      nombre: 1,
+      curseur: '*'
+    },
+    headers: {
+      Authorization: 'Bearer ' + processingConfig.apiSireneAccessToken
+    }
+  })).data
+  console.log(sireneApiRes.etablissements[0])
 }
 
 // used to manage interruption
--- test/test.js
+++ test/test.js
@@ -1,10 +1,8 @@
 process.env.NODE_ENV = 'test'
 const config = require('config')
-const axios = require('axios')
-const chalk = require('chalk')
-const moment = require('moment')
 const express = require('express')
 const assert = require('assert').strict
+const testUtils = require('@data-fair/processings-test-utils')
 const sireneProcessing = require('../')
 
 describe('Hello world processing', () => {
@@ -13,7 +11,7 @@ describe('Hello world processing', () => {
   app.use(express.static('test/resources'))
 
   before('serve test resources', (cb) => {
-    server = app.listen(8087, cb)
+    server = app.listen(8097, cb)
   })
 
   after('stop serving rest resources', (cb) => {
@@ -33,45 +31,30 @@ describe('Hello world processing', () => {
   it('should run a task', async function () {
     this.timeout(36000000)
 
-    const axiosInstance = axios.create({
-      baseURL: config.dataFairUrl,
-      headers: { 'x-apiKey': config.dataFairAPIKey }
-    })
-    // customize axios errors for shorter stack traces when a request fails
-    axiosInstance.interceptors.response.use(response => response, error => {
-      if (!error.response) return Promise.reject(error)
-      delete error.response.request
-      error.response.config = { method: error.response.config.method, url: error.response.config.url, data: error.response.config.data }
-      return Promise.reject(error.response)
-    })
-    const pluginConfig = { pluginMessage: 'Hello' }
-    const processingConfig = {
-      datasetMode: 'create',
-      dataset: { title: 'Sirene test' },
-      stockUrl: 'http://localhost:8087/stock.zip'
-    }
-    const log = {
-      step: (msg) => console.log(chalk.blue.bold.underline(`[${moment().format('LTS')}] ${msg}`)),
-      error: (msg, extra) => console.log(chalk.red.bold(`[${moment().format('LTS')}] ${msg}`), extra),
-      warning: (msg, extra) => console.log(chalk.red(`[${moment().format('LTS')}] ${msg}`), extra),
-      info: (msg, extra) => console.log(chalk.blue(`[${moment().format('LTS')}] ${msg}`), extra),
-      debug: (msg, extra) => {
-        // console.log(`[${moment().format('LTS')}] ${msg}`, extra)
-      }
-    }
-    const patchConfig = async (patch) => {
-      console.log('received config patch', patch)
-      Object.assign(processingConfig, patch)
-    }
-    await sireneProcessing.run({ pluginConfig, processingConfig, axios: axiosInstance, log, patchConfig, tmpDir: './data/test' })
-    assert.equal(processingConfig.datasetMode, 'update')
-    assert.equal(processingConfig.dataset.title, 'Sirene test')
-    const datasetId = processingConfig.dataset.id
-    assert.ok(datasetId.startsWith('sirene-test'))
+    const context = testUtils.context({
+      processingConfig: {
+        datasetMode: 'create',
+        dataset: { title: 'Sirene établissements test' },
+        // datasetMode: 'update',
+        // dataset: { id: 'sirene-etablissements-test-3', title: 'Sirene établissements test' },
+        stockUrl: 'http://localhost:8097/stock.zip',
+        apiSireneAccessToken: config.apiSireneAccessToken
+      },
+      tmpDir: './data/test'
+    }, config, false)
+    try {
+      await sireneProcessing.run(context)
+      assert.equal(context.processingConfig.datasetMode, 'update')
+      assert.equal(context.processingConfig.dataset.title, 'Sirene établissements test')
+      const datasetId = context.processingConfig.dataset.id
+      assert.ok(datasetId.startsWith('sirene-etablissements-test'))
 
-    // another execution should update the dataset, not create it
-    await new Promise(resolve => setTimeout(resolve, 4000))
-    await sireneProcessing.run({ pluginConfig, processingConfig, axios: axiosInstance, log, patchConfig })
-    assert.equal(processingConfig.dataset.id, datasetId)
+      // another execution should update the dataset, not create it
+      await new Promise(resolve => setTimeout(resolve, 4000))
+      await sireneProcessing.run(context)
+      assert.equal(context.processingConfig.dataset.id, datasetId)
+    } finally {
+      if (context.ws._ws) context.ws._ws.terminate()
+    }
   })
 })
